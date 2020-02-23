48ac7b7c83f3903eff0d43d5cd4b9613dd74c176
marcelojanke <marcelosjanke@icloud.com>
2020-02-23 03:10:17 -0300

    init Commit

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const app = require('express')()
 const bd = require('body-parser')
 const router = require('./router')
-
+require('dotenv').config()
 
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/test', {useNewUrlParser : true,useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://root:Valdisnei123@cluster0-aqjra.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser : true,useUnifiedTopology: true})
 
 
 app.use(bd.urlencoded({extended : false})).use(bd.json()).use(router)
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log('hello')
 })
